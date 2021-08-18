9b30c4060e8a770f0972f489c656a742b7852b3a
Suyeon <alveloper@gmail.com>
2021-08-18 19:33:39 +0900

    Child components and dot notation

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { Button, StyleSheet, Text, View } from "react-native";
-import Modal from "react-native-modal";
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "../components/Button";
+import { Modal } from "../components/Modal";
 
 export default function TabOneScreen() {
 
@@ -14,10 +15,15 @@ export default function TabOneScreen() {
       <View style={styles.separator} />
       <Button title="button" onPress={handleModal} />
       <Modal isVisible={isModalVisible}>
-        <View style={{ flex: 1 }}>
-          <Text>Hello!</Text>
-          <Button title="Hide modal" onPress={handleModal} />
-        </View>
+        <Modal.Container>
+          <Modal.Header title="LogRocket is fab!" />
+          <Modal.Body>
+            <Text style={styles.text}>Agree to continue with this guide</Text>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button title="I agree" onPress={handleModal} />
+          </Modal.Footer>
+        </Modal.Container>
       </Modal>
     </View>
   );
